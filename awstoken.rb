9c50d2f08eacff39a1e89fe997072649d22ee351
Ivan Vandot <ivan@vandot.rs>
2018-04-12 12:14:02 +0200

    change sha256

--- awstoken.rb
+++ awstoken.rb
@@ -2,7 +2,7 @@ class Awstoken < Formula
   desc "Bash wrapper around awscli for easy temporary security credential generation for AWS API"
   homepage "https://github.com/vandot/awstoken"
   url "https://github.com/vandot/awstoken/archive/v0.1.0.tar.gz"
-  sha256 "6c8389c344dcdb9455e4d8bbddaa41de5d2cb6681d2d947bcbf01501bc4a458e"
+  sha256 "c8d886bebc3dcd5b61f0babf9e028199d28500582675a8080eeefa1b3ab949f1"
 
   depends_on "awscli"
   depends_on "jq"
